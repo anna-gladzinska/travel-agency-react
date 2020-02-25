3ee652278bf8db8ef0daf0600f2e64d97d32d0b3
Anna Gladzińska <agladzinska@gmail.com>
2020-02-25 18:55:21 +0100

    Add trip options

---
+++ src/components/features/OrderOption/OrderOptionIcons.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './OrderOption.scss';
+import { formatPrice } from '../../../utils/formatPrice';
+
+import Icon from '../../common/Icon/Icon';
+
+const OrderOptionIcons = ({ values, setOptionValue, required, currentValue }) => (
+  <div
+    className={styles.icon}
+  >
+    {required ? '' : (
+      <div
+        onClick={() => setOptionValue('')}
+      >
+        <Icon name='times-circle' /> none
+      </div>
+    )}
+    {values.map(value => (
+      <div
+        className={styles.icon + (currentValue === value.id ? ' ' + styles.iconActive : '')}
+        key={value.id}
+        onClick={() => setOptionValue(value.id)}
+      >
+        <Icon name={value.icon} />{value.name} ({formatPrice(value.price)})
+      </div>
+    ))}
+  </div>
+);
+
+OrderOptionIcons.propTypes = {
+  values: PropTypes.array,
+  required: PropTypes.bool,
+  currentValue: PropTypes.string,
+  setOptionValue: PropTypes.func,
+};
+
+export default OrderOptionIcons;
