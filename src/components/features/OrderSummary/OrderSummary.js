6046822e380063c3ecaa6e01a36b26ecaefa2f74
Anna Gladzińska <agladzinska@gmail.com>
2020-02-27 19:48:45 +0100

    Fix order summary format

--- src/components/features/OrderSummary/OrderSummary.js
+++ src/components/features/OrderSummary/OrderSummary.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import styles from './OrderSummary.scss';
 import { calculateTotal } from '../../../utils/calculateTotal';
 import { formatPrice } from '../../../utils/formatPrice';
+import PropTypes from 'prop-types';
 
 const OrderSummary = ({ cost, options }) => (
-  <h2 className={styles.component}>Total:<strong> {calculateTotal(formatPrice(cost), options)}</strong></h2>
+  <h2 className={styles.component}>Total: <strong>{formatPrice(calculateTotal(cost, options))}</strong></h2>
 );
 
 OrderSummary.propTypes = {
